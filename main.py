60a2843b4d45854cb42633a5cb205e7dcbad10ec
Geoff <geoff.e.miller@gmail.com>
2015-12-27 15:55:28 -0700

    1

---
+++ main.py
@@ -0,0 +1,16 @@
+# Author Geoff Miller
+# Date: 12-26-15
+# Currency Problem
+
+
+class Currency:
+    """Defines a currency"""
+    pass
+
+
+class Solution:
+    """Defines a solution"""
+    pass
+
+
+
